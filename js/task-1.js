de9dea4c784c81e94cdd27ecacb7231faec8bc47
Oleg Gaponenko <oleggaponenko@gmail.com>
2025-03-16 00:49:22 +0200

    add changes to task 1

--- js/task-1.js
+++ js/task-1.js
@@ -1,1 +1,11 @@
-const totalCategories = document.querySelector('.item');
+const totalCategories = document.querySelectorAll('.item');
+
+console.log(`Number of categories: ${totalCategories.length}`);
+
+totalCategories.forEach((category) => {
+    const title = category.querySelector('.title').textContent;
+    console.log(`Category: ${title}`);
+
+    const numberOfElemets = category.querySelectorAll('.category-item'); 
+    console.log(`Elements: ${numberOfElemets.length}`);
+});
