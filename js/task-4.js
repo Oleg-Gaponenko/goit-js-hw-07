197aff712bdd6fae70032d3e354f44ec8d31aa08
Oleg Gaponenko <oleggaponenko@gmail.com>
2025-03-16 16:00:18 +0200

    add changes to the fourth task

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,18 @@
+const userForm = document.querySelector('.login-form');
 
+userForm.addEventListener('submit', handleSubmit);
+
+function handleSubmit(event) {
+    event.preventDefault();
+
+    const userPassword = event.target.elements.password.value.trim();
+    const userEmail = event.target.elements.email.value.trim();
+    const userData = {};
+
+    if(userPassword === '' || userEmail === '' || userEmail !== userEmail.includes('@')) {
+        alert `All form fields must be filled in`;
+    } else {
+        userData.email = userEmail;
+        userData.password = userPassword;
+    }
+}
