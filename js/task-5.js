f6b68aa1309181471c4059b9a25944574bfb07e1
Oleg Gaponenko <oleggaponenko@gmail.com>
2025-03-16 17:23:31 +0200

    add changes to task 5

--- js/task-5.js
+++ js/task-5.js
@@ -3,3 +3,15 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const background = document.querySelector('body');
+const button = document.querySelector('.change-color');
+const colorText = document.querySelector('.color')
+
+button.addEventListener('click', handleClick);
+
+function handleClick() {
+  const newColor = getRandomHexColor();
+  background.style.backgroundColor = newColor;
+  colorText.textContent = newColor;
+}
